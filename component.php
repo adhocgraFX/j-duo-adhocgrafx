01d7c6ec668a60f443c1d7b488b278cdffde95fd
adhocgraFX <info@adhocgrafx.de>
2015-01-26 18:53:31 +0100

    css back to left

--- component.php
+++ component.php
@@ -7,9 +7,11 @@ $tpath = $this->baseurl.'/templates/'.$this->template;
 // generator tag
 $this->setGenerator(null);
 
+// Add JavaScript Frameworks
+// JHtml::_('bootstrap.framework');
+
 // load sheets and scripts
 $doc->addStyleSheet($tpath.'/css/j-template.css');
-
 ?>
 
 <!doctype html>
@@ -20,7 +22,6 @@ $doc->addStyleSheet($tpath.'/css/j-template.css');
     <!-- typekit fonts -->
     <script type="text/javascript" src="//use.typekit.net/zdh2try.js"></script>
     <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/print.css" type="text/css" media="print"/>
     <jdoc:include type="head" />
 </head>
